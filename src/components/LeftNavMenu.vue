54a90557d732f4dd87083bd414245fa57a31c740
IvanSanczewski <isanczewski@gmail.com>
2022-10-17 22:00:39 +0300

    Add project

---
+++ src/components/LeftNavMenu.vue
@@ -0,0 +1,20 @@
+<template>  
+    <div class="left">
+        <h1> {{ title }}</h1>
+        <ul>
+            <li>
+                <!-- implement v-fot with menu objects -->
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LeftNavMenu'
+}
+</script>
+
+<style>
+
+</style>
