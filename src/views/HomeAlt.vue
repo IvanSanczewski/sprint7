f849751a1c13e0f3f6140ca256d27b49197fc860
IvanSanczewski <isanczewski@gmail.com>
2022-10-21 18:29:50 +0300

    Ex1 -- HomeAlt.vue - include id parameter in services array, include onclick event & toggleService method with selected object as an argument, change its boolean value and filter array to get only the objects with true value in service in the new array. Call sumPrice method with the filtered array as an argument and aply forEach method to calculate the total price (restarting the totalPrice is required before the method so it does not accumulate previous values. Ex1 WORKING

--- src/views/HomeAlt.vue
+++ src/views/HomeAlt.vue
@@ -2,11 +2,13 @@
     <form class="budget-box">
         <p>Choose from the list bellow to create your budget</p>
         <ul>
-            <li v-for="item, service in services" :key="service">
-                <input type="checkbox" v-model="item.service" :value="item.price">
+            <li v-for="item, id in services" :key="id">
+                <!-- <input type="checkbox" v-model="item.service" :value="item.price" @click="sumPrice(item.service, item.price)"> -->
+                <input type="checkbox" v-model="item.service" :value="item.price" @click="toggleService(item)">
                 {{ item.text}}
             </li>
         </ul>
+        <p> Tota price: {{ totalPrice }} Eur</p>
     </form>
 </template>
 
@@ -16,15 +18,30 @@ export default {
     data() {
         return {
             services: [
-                {service: 'web', text:'Website (500 €)' , price: 500},
-                {service: 'seo', text:'SEO Consulting (300 €)' , price: 200},
-                {service: 'ads', text:'Google Ads Campaign (200 €)' , price: 300},
+                {id: 1, service: false, text:'Website (500 €)', price: 500},
+                {id: 2, service: false, text:'SEO Consulting (300 €)', price: 300},
+                {id: 3, service: false, text:'Google Ads Campaign (200 €)', price: 200},
             ],
+            includedServices: [],
             totalPrice: 0
         }
+    },
+    methods: {
+        toggleService(item) {
+            // al pasar item como argumento desde el evento todas las instrucciones hay que hacerlas a partir de item.loQueSea
+            item.service = !item.service
+            this.includedServices = this.services.filter((item) => {
+                return item.service === true
+            })
+            this.sumPrice(this.includedServices)
+        },
+        sumPrice(includedServices) {
+            this.totalPrice = 0
+            includedServices.forEach ((item) => {
+                this.totalPrice += item.price     
+            })
+        }
     }
-
-
 }
 </script>
 
