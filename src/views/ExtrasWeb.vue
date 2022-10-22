35d0f37483e2c27c73d01001a9b43002e7f58b5f
IvanSanczewski <isanczewski@gmail.com>
2022-10-22 17:15:11 +0300

    Ex2 -- Create ExtrasWeb.vue component to host languages & pages extras template and data to be passed to Home (parent) to calculate

---
+++ src/views/ExtrasWeb.vue
@@ -0,0 +1,27 @@
+<template>
+    <form class="extras">
+        <p>
+            How many pages do you need?
+            <input type="number" v-model="pages">
+        </p>
+        <p>
+            In how many different languages?
+            <input type="number" v-model="languages">
+        </p>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pages: 1,
+            languages: 1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
