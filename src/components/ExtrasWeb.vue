bfa7d96e4676af3416ea32ef1e16878ea1f1a377
IvanSanczewski <isanczewski@gmail.com>
2022-10-23 22:51:30 +0300

    Ex2 -- HomeAlt.vue - add data for languages, pages & extraWeb and pass it to ExtrasWeb component. ExtrasWeb.vue - recieve Props, add extrasPrice data and function to calculate extra costs

--- src/components/ExtrasWeb.vue
+++ src/components/ExtrasWeb.vue
@@ -1,28 +1,47 @@
 <template>
     <form class="extras">
-        <p>
-            How many pages do you need?
-            <input type="number" v-model="pages">
-        </p>
-        <p>
-            In how many different languages?
-            <input type="number" v-model="languages">
-        </p>
+        <div>
+            <label for="pages">How many pages do you need?</label>
+            <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
+            <input type="number" v-model="pages" @click="sumExtrasPrice">
+        </div>
+        <div>
+            <label for="pages">In how many different languages?</label>
+            <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
+            <input type="number" v-model="languages" @click="sumExtrasPrice">
+            <p> precio extra: {{ extrasPrice }} eur </p>
+        </div>
     </form>
 </template>
 
 <script>
 export default {
+    // props: ['pages', 'languages'],
+    props: ['pages', 'languages', 'extraWeb'],
     data() {
         return {
-            pages: 1,
-            languages: 1
+            extrasPrice: 0
         }
-    }
+    },
+    methods:{
+        //PASAR EL MÉTODO A HOME Y LLAMARLO USANDO EMITS, USAR WATCH PARA VER CUÁNDO EL USUARIO CAMBIA VALORES
+        sumExtrasPrice(){
+            if (this.pages <= 1 && this.languages <= 1) {
+                this.extrasPrice = 0
+            } else {
+                this.extrasPrice = this.pages * this.languages * this.extraWeb
+            }
+
+        }
+    },
+    // watch: {
+
+    // }
+
 }
 </script>
-
 <style>
+
 .extras {
     width: auto;
 
@@ -31,10 +50,5 @@ export default {
 
     margin: 1em 0;
     padding: 1em;
-}
-
-p {
-    display: block;
-    width: fit-content;
 }
 </style>
