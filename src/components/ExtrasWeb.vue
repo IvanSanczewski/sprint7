9444869f4bb5dafa1d936bd97791a0793c57ced3
IvanSanczewski <isanczewski@gmail.com>
2022-10-25 12:32:51 +0300

    Ex3 -- main.js - import & mount awesomefonts & add  + - icons in library // ExtrasWeb.vue - add icons (styling needed)  // import dependencies in .json files

--- src/components/ExtrasWeb.vue
+++ src/components/ExtrasWeb.vue
@@ -1,15 +1,30 @@
 <template>
-    <form class="extras">
-        <div>
-            <label for="pages">How many pages do you need?</label>
-            <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
-            <input type="number" v-model="pages" @click="addExtras">
-        </div>
-        <div>
-            <label for="pages">In how many different languages?</label>
-            <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
-            <input type="number" v-model="languages" @change="addExtras">
-            <!-- <p> Extra Webs Price: {{ extrasPrice }} Eur </p> -->
+    <form>
+        <div class="extras">
+            <div>
+                <div>
+                    <label for="pages">How many pages do you need?</label>
+                </div>
+                <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
+                <div class="btn-block">
+                    <font-awesome-icon icon="fa-solid fa-plus" class="product-btn"/>
+                    <input type="text" v-model="pages" @click="addExtras">
+                    <font-awesome-icon icon="fa-solid fa-minus" class="product-btn"/>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <label for="pages">In how many different languages?</label>
+                </div>
+                <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
+                <!-- <font-awesome-icon icon="fa-solid fa-plus"> -->
+                <div class="btn-block">
+                    <font-awesome-icon icon="fa-solid fa-plus" class="product-btn"/>
+                    <input type="text" v-model="languages" @change="addExtras" size="5">
+                    <font-awesome-icon icon="fa-solid fa-minus" class="product-btn"/>
+                </div>
+            </div>
+
         </div>
     </form>
 </template>
@@ -17,7 +32,7 @@
 <script>
 
 export default {
-
+    name: 'ExtrasWeb',
     props: ['pages', 'languages', 'extraWeb'],
     emits: ['sumUpExtras'],
     watch: {
@@ -48,13 +63,34 @@ export default {
     border: 3px solid black;
     border-radius: 1em;
     
-}
-.extras div{
     display: flex;
     flex-flow: row wrap;
     justify-content: space-between;
-    column-gap: 1.5em;
-
+    
     margin: .5em 0;
 }
+
+input {
+    width: 1em;
+    margin: 0 .5em;
+    text-align: center;
+
+    border: none;
+}
+
+.product-btn {
+    width: 2em;
+    height: 2em;
+    padding: .5em;
+
+    font-size: .65em;
+    font-weight: 400;
+    
+    color: white;
+    background: coral;
+
+    border: none;
+    border-radius: .5em;
+}
+
 </style>
