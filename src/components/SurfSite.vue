9bf497e4ec669d0173f2b21a6d2eef5aac34f2d2
IvanSanczewski <isanczewski@gmail.com>
2022-10-26 22:30:53 +0300

    Ex4 -- create SurfSite.vue component with buttons template and navigation emits // HomeAlt.vue - import SurfSite component, add to components & link it in template (all commented) // routing needed to navigate the site; decide whether navigation is through components or views; decide whether main form is shown though V-IF, V-ELSE, V-SHOW or navigation

---
+++ src/components/SurfSite.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-btn">
+        <button @click="previousSentence">Backwards</button>
+        <button @click="nextSentence">Forward</button>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: SurfSite,
+    methods: {
+        previousPage() {
+            this.$emit('previous')
+        },
+        nextPage() {
+            this.$emit('next')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
