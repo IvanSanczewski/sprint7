456b8144f6991a7566c51646e363daacb74dd704
IvanSanczewski <isanczewski@gmail.com>
2022-10-26 11:56:42 +0300

    Ex3 -- ExtrasWeb.vue - comment unnecessary template calls for ExtraConter component as it is now HomeAlt.vue's child; comment unnecessary props, emits, watches, data & methods from scripts

--- src/components/ExtrasWeb.vue
+++ src/components/ExtrasWeb.vue
@@ -1,54 +1,68 @@
 <template>
     <form>
         <div class="extras">
-            <div>
-                <div>
+            <!-- <div> -->
+                <!-- <div> -->
                     <label for="pages">How many pages do you need?</label>
-                </div>
+                    <!-- </div> -->
+                    <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
+                    <!-- <div class="btn-block"> -->
+                        <!-- <input type="text" v-model="pages" @click="addExtras"> -->
+                        <!-- </div> -->
+                        <!-- </div> -->
+                        <!-- <div> -->
+                            <!-- <div> -->
+                    <label for="languages">In how many different languages?</label>
+                    
+                    <!-- cómo colocarlo en un lugar específico -->
+                    <!-- <ExtrasCounter :pages="pages" @addOnePage="addOnePageMore"/> -->
+                    <!-- <ExtrasCounter :pages="pages" :languages="languages" @addOnePage="addOnePageMore"/> -->
+                    <!-- cómo colocarlo en un lugar específico -->
+                                
+                <!-- </div> -->
                 <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
-                <div class="btn-block">
-                    <font-awesome-icon icon="fa-solid fa-plus" class="product-btn"/>
-                    <input type="text" v-model="pages" @click="addExtras">
-                    <font-awesome-icon icon="fa-solid fa-minus" class="product-btn"/>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <label for="pages">In how many different languages?</label>
-                </div>
-                <!-- min="1" for the input does not avoid user's ability to manually insert negative numbers, this is handled in the method that calculates extras -->
-                <!-- <font-awesome-icon icon="fa-solid fa-plus"> -->
-                <div class="btn-block">
-                    <font-awesome-icon icon="fa-solid fa-plus" class="product-btn"/>
-                    <input type="text" v-model="languages" @change="addExtras" size="5">
-                    <font-awesome-icon icon="fa-solid fa-minus" class="product-btn"/>
-                </div>
-            </div>
-
+                <!-- <div class="btn-block"> -->
+                    <!-- <input type="text" v-model="languages" @change="addExtras"> -->
+                <!-- </div> -->
+            <!-- </div> -->
         </div>
     </form>
 </template>
 
 <script>
+// import ExtrasCounter from '@/components/ExtrasCounter.vue'
 
 export default {
     name: 'ExtrasWeb',
-    props: ['pages', 'languages', 'extraWeb'],
-    emits: ['sumUpExtras'],
-    watch: {
-        pages() {
-            this.addExtras()
-        },
-        languages() {
-            this.addExtras()
-        },
-    },
-    methods: {
+    // components: { ExtrasCounter },
+    // extraWeb is not passed as a prop because it is not used in this component
+    // props: ['pages', 'languages'],
+    // props: ['pages', 'languages', 'extraWeb'],
+    // emits: ['sumUpExtras'],
+    // watch: {
+    //     pages() {
+    //         this.addExtras()
+    //     },
+    //     languages() {
+    //         this.addExtras()
+    //     },
+    // },
+    // data() {
+    //     return {
+    //         updatedPages: 0,
+    //     }
+    // },
+    // methods: {
         //PASAR EL MÉTODO A HOME Y LLAMARLO USANDO EMITS, USAR WATCH PARA VER CUÁNDO EL USUARIO CAMBIA VALORES
-        addExtras() {
-            this.$emit('sumUpExtras', this.pages, this.languages)
-        } 
-    }
+        // addExtras() {
+        //     this.$emit('sumUpExtras', this.pages, this.languages)
+            // this.$emit('sumUpExtras', this.languages)
+        // },
+        // addOnePageMore(){
+        //     this.updatedpages++
+        //     console.log(this.updatedPages)
+        // }
+    // }
 }
     
 </script>
@@ -64,14 +78,15 @@ export default {
     border-radius: 1em;
     
     display: flex;
-    flex-flow: row wrap;
+    flex-flow: column wrap;
+    /* flex-flow: row wrap; */
     justify-content: space-between;
     
     margin: .5em 0;
 }
 
 input {
-    width: 1em;
+    max-width: 3em;
     margin: 0 .5em;
     text-align: center;
 
