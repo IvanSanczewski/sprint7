11b06e30a0584c182d846aeef3fd2760c9713058
IvanSanczewski <isanczewski@gmail.com>
2022-11-01 14:53:35 +0200

    Ex7 -- Budgets.vue - add budgets lists with v-if according to the buttons & change flags in either methods in order to show selected list view, create resetBudgetsList method deleting all components new arrays, change flags & emit reset custom event

--- src/components/Budgets.vue
+++ src/components/Budgets.vue
@@ -1,12 +1,11 @@
 <template>
-    <!-- IMPLEMENTAR UN V-FOR PARA MOSTRARLOS??? -->    
     <div class="budget-list">
         <h2>Budgets List:</h2>
         
         <div class="budgets-nav">
             <button @click="displayAZ"> Alphabetical Order </button>
             <button @click="displayByDate"> Creation Order </button>
-            <button @click="displayBudgetList"> Reset Order </button>
+            <button @click="resetBudgetsList"> Reset Order </button>
         </div>
         
         <!-- <ul>
@@ -15,8 +14,32 @@
             </li>
         </ul> -->
 
-        <ul>
+        <ul v-if="noSort">
             <li v-for="item, id in budgetsList" :key="id">
+                <p>Client: <strong>{{ item.client }}</strong>  -  Budget: <strong>{{ item.name }}</strong></p>
+                <!-- <h5>Services included: {{ item.service }}
+                        <li v-for="item, id in ITEM.SERVICE" :key="id">
+                            {{ item.text }}
+                        </li>
+                    </h5> -->
+                <p>Budget Total Price: <strong>{{ item.budgetPrice }}</strong> Eur</p>
+                <div class="separator"></div>
+            </li>
+        </ul>
+        <ul v-if="azSort">
+            <li v-for="item, id in budgetsListAZ" :key="id">
+                <p>Client: <strong>{{ item.client }}</strong>  -  Budget: <strong>{{ item.name }}</strong></p>
+                <!-- <h5>Services included: {{ item.service }}
+                        <li v-for="item, id in ITEM.SERVICE" :key="id">
+                            {{ item.text }}
+                        </li>
+                    </h5> -->
+                <p>Budget Total Price: <strong>{{ item.budgetPrice }}</strong> Eur</p>
+                <div class="separator"></div>
+            </li>
+        </ul>
+        <ul v-if="dateSort">
+            <li v-for="item, id in budgetsListByDate" :key="id">
                 <p>Client: <strong>{{ item.client }}</strong>  -  Budget: <strong>{{ item.name }}</strong></p>
                 <!-- <h5>Services included: {{ item.service }}
                         <li v-for="item, id in ITEM.SERVICE" :key="id">
@@ -35,15 +58,21 @@
 export default {
     name: 'Budgets',
     props: ['budgetsList'],
-    // ASYNCRONOUS ERROR CALL 
+    emits: ['reset'],
     data() {
         return {
+            noSort: true,
+            azSort: false,
+            dateSort: false,
             budgetsListAZ: [],
             budgetsListByDate: [],
         }
     },
     methods: {
         displayAZ() {
+            this.noSort = false
+            this.dateSort = false
+            this.azSort = true
             this.budgetsListAZ = [...this.budgetsList]
             .sort(function(a, z) {
                 if (a.name < z.name ) return -1;
@@ -55,6 +84,9 @@ export default {
             console.log(this.budgetsListAZ);
         },
         displayByDate() {
+            this.noSort = false
+            this.azSort = false
+            this.dateSort = true
             this.budgetsListByDate = [...this.budgetsList]
             .sort(function(a, z) {
                 if (a.date < z.date ) return -1;
@@ -65,8 +97,16 @@ export default {
             console.log(this.budgetsList);
             console.log(this.budgetsListByDate);
         },
-        displayBudgetList() {
-            alert('Reset Order')
+        resetBudgetsList() {
+            this.$emit('reset')
+            this.budgetsListAZ = []
+            this.budgetsListByDate = []
+            this.noSort = true
+            this.azSort = false
+            this.dateSort = false
+            console.log(this.budgetsList);
+            console.log(this.budgetsListAZ);
+            console.log(this.budgetsListByDate);
         },
     }
 }
@@ -79,8 +119,6 @@ export default {
     flex-flow: column wrap;
     text-align: left;
 }
-
-
 
 .separator {
     width: 100%;
